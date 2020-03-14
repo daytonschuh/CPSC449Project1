a6b62ce518c5d66f0bd7fa2ff3ff36f5f113bf41
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-03-14 10:44:00 -0700

    Add files via upload

---
+++ testpost.sh
@@ -0,0 +1,6 @@
+curl    --verbose \
+        --request \
+        --header 'Content-type: application/json' \
+        --data @testpost.json \
+        http://localhost:5000/create-post
+
---
+++ testuser.sh
@@ -0,0 +1,5 @@
+curl    --verbose \
+	--header 'Content-Type: application/json' \
+        --data @testuser.json \
+        http://localhost:5000/register
+
