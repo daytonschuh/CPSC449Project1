2418d28d566b4323e9296d34d2ab493c8d348eac
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-03-29 09:49:18 -0700

    Finalizing

--- autostart.sh
+++ autostart.sh
@@ -8,7 +8,7 @@ sudo apt install --yes gunicorn
 sudo pip3 install marshmallow-sqlalchemy
 sudo pip3 install pytz
 ulimit -n 8192 &
-xterm -e caddy &
-xterm -title "Foreman" -e bash -c "foreman start -m posts=3,accounts=3"
-xterm -title "TestPost" -e bash -c "sleep 1; bash testpost.sh; bash"
+xterm -title "Caddy" -e caddy &
+xterm -title "Foreman" -e bash -c "foreman start -m posts=3,accounts=3" &
+xterm -title "TestPost" -e bash -c "sleep 1; bash testpost.sh; bash" &
 xterm -title "TestUser" -e bash -c "sleep 1; bash testuser.sh; bash"
