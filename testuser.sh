1e3f0d9c6a74d30e04cd097eb6c8a0a36b8e519b
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-04-20 04:59:54 -0700

    simplifying

--- testuser.sh
+++ testuser.sh
@@ -1,27 +1,25 @@
 #Create a user
-curl    --X POST \
-	--header 'Content-Type: application/json' \
+curl    --header 'Content-Type: application/json' \
         --data @testuser.json \
         http://localhost:5000/register
 
 #Update email
-curl	--X PUT \
+curl	-X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/update_email
 
 #Increment Karma
-curl	--X PUT \
+curl	-X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/increment_karma
 
 #Decrement Karma
-curl	--X PUT \
+curl	-X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/decrement_karma
 
 #Deactivate Account
-curl	--X DELETE \
-	http://localhost:5000/deactivate_account/test_user
+curl	http://localhost:5000/deactivate_account/test_user
