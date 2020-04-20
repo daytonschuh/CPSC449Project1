207d0f79aa41d19fbd3c638d4fe5414877d1904a
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-04-20 04:24:50 -0700

    Removing verbose to increase readability

--- testpost.sh
+++ testpost.sh
@@ -1,26 +1,21 @@
 #Create a post
-curl    --verbose \
-        --X POST \
+curl    --X POST \
         --header 'Content-type: application/json' \
         --data @testpost.json \
         http://localhost:5000/create_post
 
 #Retrieve a post
-curl	--verbose \
-	--X GET \
+curl	--X GET \
 	http://localhost:5000/retrieve_post/2147483647
 
 #Delete a post
-curl	--verbose \
-	--X DELETE \
+curl	--X DELETE \
 	http://localhost:5000/delete_post/2147483647
 
 #Get n most recent posts from a community
-curl	--verbose \
-	--X GET \
+curl	--X GET \
 	http://localhost:5000/list_posts_comm/News/3
 
 #Get n most recent posts to any community
-curl	--verbose \
-	--X GET \
+curl	--X GET \
 	http://localhost:5000/list_posts/3
