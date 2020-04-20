daeb6e3bc44c0aa7338197013fa31df7bfb5cb5a
daytonschuh <47435241+daytonschuh@users.noreply.github.com>
2020-04-20 04:25:43 -0700

    Removing verbose to increase readability

--- testuser.sh
+++ testuser.sh
@@ -1,32 +1,27 @@
 #Create a user
-curl    --verbose \
-	-X POST \
+curl    --X POST \
 	--header 'Content-Type: application/json' \
         --data @testuser.json \
         http://localhost:5000/register
 
 #Update email
-curl	--verbose \
-	-X PUT \
+curl	--X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/update_email
 
 #Increment Karma
-curl	--verbose \
-	-X PUT \
+curl	--X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/increment_karma
 
 #Decrement Karma
-curl	--verbose \
-	-X PUT \
+curl	--X PUT \
 	--header 'Content-Type: application/json' \
 	--data @testuser.json \
 	http://localhost:5000/decrement_karma
 
 #Deactivate Account
-curl	--verbose \
-	-X DELETE \
+curl	--X DELETE \
 	http://localhost:5000/deactivate_account/test_user
